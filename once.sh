0e115255091e5416eded998b9aa4425e2ff94566
Herman van Dompseler <herman@dompseler.nl>
2020-02-10 08:44:26 +0100

    admin access test

--- once.sh
+++ once.sh
@@ -6,3 +6,8 @@ echo "Create new database password:"
 dbpwd=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 printf $dbpwd | docker secret create ltilauncher_db_dba_password -
 echo done...
+
+echo "Create new admin password:"
+admpwd=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
+printf $admpwd | docker secret create ltilauncher_admin_password -
+echo done...
